1a59837a7cd7f12739cf793ff395669edfbe916b
Paula Livingstone <paula@paulalivingstone.com>
2017-10-29 13:47:46 +0000

    Update met-office-datapoint-scraper.py
    Parses received data and outputs text conversion of met office weather codes

--- met-office-datapoint-scraper.py
+++ met-office-datapoint-scraper.py
@@ -1,13 +1,19 @@
 import lxml
 from bs4 import BeautifulSoup
 import urllib.request
+codes={'NA':'Not available','0': 'Clear night','1': 'Sunny day','2': 'Partly cloudy (night)','3': 'Partly cloudy (day)','4': 'Not used','5': 'Mist','6': 'Fog','7': 'Cloudy','8': $
+final=[]
 
-with urllib.request.urlopen("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/3136?res=3hourly&key=---your API KEY here---") as url:
+with urllib.request.urlopen("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/3136?res=3hourly&key=3a169d7f-320c-40f6-8ffb-2078e513b24d") as url:
     s = url.read()
 
 y=BeautifulSoup(s, "lxml")
+y=str(y)
+weather_report=[element.strip('<>') for element in y.split('<')]
 
-brian=y.findAll('rep') #captures all reports from xml and adds them to a list called brian
+for item in weather_report:
+    if 'rep d' in item:
+        final.append(item.split(' ')[-1])
+weather=final[1].split('"')[1]
 
-for elem in brian: #print out list brian on separate lines
-    print (elem)
+print(codes[weather])
